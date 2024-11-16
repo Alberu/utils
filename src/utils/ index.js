ef167d6d5d0b29edc8bb15d5599d9562e5713891
Alberu <treblabr@gmail.com>
2024-11-16 16:50:20 +0000

    some minor setup

---
+++ src/utils/ index.js
@@ -0,0 +1,14 @@
+export const pages = [
+  {
+    id: "memory",
+    title: "Memory Match",
+    description: "Match pairs of cards to win",
+    thumbnail: "/api/placeholder/300/200",
+  },
+  {
+    id: "snake",
+    title: "Snake Game",
+    description: "Classic snake game - eat food and grow longer",
+    thumbnail: "/api/placeholder/300/200",
+  },
+];
