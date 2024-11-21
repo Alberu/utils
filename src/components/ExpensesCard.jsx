50de435cc5ff193d10f12696abd72af844540b45
Alberu <treblabr@gmail.com>
2024-11-21 23:05:52 +0000

    added changing the expense name functionality

--- src/components/ExpensesCard.jsx
+++ src/components/ExpensesCard.jsx
@@ -7,6 +7,7 @@ import CircularPieChart from "./CircularPieChart"
 import { Button } from "./ui/button"
 import { AddExpense } from "./AddExpense"
 import { ColourPicker } from "./ColourPicker"
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover"
 
 const ExpensesCard = ({ salary, expenses, setExpenses }) => {
     const handleUpdateExpense = (expenseIndex, newValue, type) => {
@@ -69,8 +70,25 @@ const ExpensesCard = ({ salary, expenses, setExpenses }) => {
                                     <div key={expenseIndex}>
                                         {/* <Label className="capitalize text-sm text-muted-foreground">{expense?.name}</Label> */}
                                         <div className="flex gap-2 items-center">
-                                            <Label className='min-w-20'>{expense?.name}</Label>
+                                            <Popover>
+                                                <PopoverTrigger asChild>
+                                                    <Button variant='ghost' style={{ color: expense?.colour }}>{expense?.name}</Button>
+                                                    {/* <Label className='min-w-20'>{expense?.name}</Label> */}
+                                                </PopoverTrigger>
+                                                <PopoverContent side="right" align="center" className='w-auto'>
+                                                    <div className="space-y-2">
+                                                        <p className="text-sm text-muted-foreground">
+                                                            Change the name of this expense.
+                                                        </p>
+                                                    <Input
+                                                        value={expense?.name}
+                                                        onChange={(e) => { handleUpdateExpense(expenseIndex, (e.target.value), 'name') }} />
+                                                    </div>
+                                                </PopoverContent>
+                                            </Popover>
+
                                             <Input
+                                                // className='border-none shadow-none' // something to keep in mind? which one looks better
                                                 type='number'
                                                 value={expense?.value}
                                                 onChange={(e) => { handleUpdateExpense(expenseIndex, Number(e.target.value), 'value') }} />
