d5d768c562fb97123eb3b83d2fb98879e9cbe655
Alberu <treblabr@gmail.com>
2024-11-21 22:31:10 +0000

    improved percent showing
    can still work on the delay when changing colours

--- src/components/CircularPieChart.jsx
+++ src/components/CircularPieChart.jsx
@@ -29,18 +29,23 @@ const CircularPieChart = ({ chartData }) => {
 
     // const totalValue = chartData.reduce((sum, item) => sum + item.value, 0)
 
-    const RADIAN = Math.PI / 180;
-    const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-        const radius = innerRadius + (outerRadius - innerRadius) * 0.3;
-        const x = cx + radius * Math.cos(-midAngle * RADIAN);
-        const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    // const RADIAN = Math.PI / 180;
+    // const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    //     const radius = innerRadius + (outerRadius - innerRadius) * 0.3;
+    //     const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    //     const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-        return (
-            <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
-                {`${(percent * 100).toFixed(0)}%`}
-            </text>
-        );
-    };
+    //     return (
+    //         <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+    //             {`${(percent * 100).toFixed(0)}%`}
+    //         </text>
+    //     );
+    // };
+
+    const renderCustomizedLabel = ({ percent }) => {
+        if (!percent) return null;
+        return `${(percent * 100).toFixed(0)}%`;
+      };
 
     return (
         <ChartContainer
