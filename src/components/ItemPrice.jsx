f4ff5337f018091b1afd8e1436e2e41b8bae303e
Alberu <treblabr@gmail.com>
2024-12-19 12:26:30 +0100

    price output implemented

--- src/components/ItemPrice.jsx
+++ src/components/ItemPrice.jsx
@@ -2,18 +2,19 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { useState } from "react";
 import { DisplayButton } from "./DisplayButton";
+import { formatCurrency } from "@/utils";
 
 export const ItemPrice = () => {
   // To store the differnet prices that you want to compare
   const [prices, setPrices] = useState([
-    { name: "Original", value: 209 },
-    { name: "New", value: 190 },
+    { name: "Original", value: 209, applyMultiplier: false },
+    { name: "New", value: 190, applyMultiplier: true },
   ]);
 
   // To store the different multipliers that you want to add
   const [multipliers, setMultipliers] = useState([
-    { name: "€->£", value: 0.83 },
-    { name: "Tax refund", value: 0.8 },
+    { name: "€->£", value: 0.83, isActive: true },
+    { name: "Tax refund", value: 0.8, isActive: true },
   ]);
 
   return (
@@ -26,10 +27,14 @@ export const ItemPrice = () => {
       <CardContent>
         <h1>Prices</h1>
         {prices.map((price, priceIndex) => (
-            <DisplayButton key={priceIndex} name={price.name} value={price.value}/>
+          <DisplayButton
+            key={priceIndex}
+            name={price.name}
+            value={price.value}
+          />
         ))}
         <h1>Multipliers</h1>
-        <ToggleGroup type="multiple">
+        <ToggleGroup type="multiple" value={multipliers} onValueChange={(newValue) => {console.log(newValue)}}>
           {multipliers.map((multiplier, multiplierIndex) => (
             <ToggleGroupItem key={multiplierIndex} value={multiplier.value}>
               {multiplier.name}
@@ -37,6 +42,16 @@ export const ItemPrice = () => {
           ))}
         </ToggleGroup>
         <h1>Savings</h1>
+        {prices.map((price, priceIndex) => (
+          <p key={priceIndex}>
+            {formatCurrency(
+              multipliers.reduce(
+                (finalPrice, multiplier) => finalPrice * multiplier.value,
+                price.value
+              )
+            )}
+          </p>
+        ))}
       </CardContent>
     </Card>
   );
