1e363fb2b121b4689123003eb458f262f4b49283
Alberu <treblabr@gmail.com>
2024-12-23 15:23:37 +0100

    fixed the yes button for will you

--- src/pages/WillYou.jsx
+++ src/pages/WillYou.jsx
@@ -9,45 +9,50 @@ const WillYou = () => {
 
   const noText = [
     "No",
-    'Are you sure',
-    'Are you really sure? 🥺',
-    '💔',
-    'Are you positive????',
-    'If you say no I will be very sad',
-    'If you say no I will be very very sad',
-    'Okay... I give up, you can say no',
-    'Just kidding just say yes',
-];
+    "Are you sure",
+    "Are you really sure? 🥺",
+    "💔",
+    "Are you positive????",
+    "If you say no I will be very sad",
+    "If you say no I will be very very sad",
+    "Okay... I give up, you can say no",
+    "Just kidding just say yes",
+  ];
   return (
     <PageLayout>
       <Card className="container">
         <CardHeader>{/* <CardTitle>Hi</CardTitle> */}</CardHeader>
-        <CardContent className="text-center">
-          <p>Will you be my valentine?</p>
-          <p className="grid-cols-2 text-center space-x-1">
-            <button
-              className="rounded-lg px-5 py-2 bg-green-400"
-              style={{ fontSize: `${8 * 1.5 ** (rejections + 1)}px` }}
-            >
-              Yes
-            </button>
-            {rejections < 40 && (
+        {!yes && (
+          <CardContent className="text-center space-y-2">
+            <p className="text-xl">Will you be my valentine?</p>
+            <p className="grid-cols-2 text-center space-x-1">
               <button
-                className="rounded-lg px-5 py-2 bg-red-400"
+                className="rounded-lg px-5 py-2 bg-green-400"
                 onClick={() => {
-                  setRejections((prevValue) => prevValue + 1);
+                    setYes(true)
                 }}
+                style={{ fontSize: `${10 * 1.4 ** (rejections + 1)}px` }}
               >
-                {noText[rejections]}
+                Yes
               </button>
-            )}
-          </p>
-          <p>Rejections: {rejections}</p>
-        </CardContent>
+              {rejections < noText.length && (
+                <button
+                  className="rounded-lg px-5 py-2 bg-red-400"
+                  onClick={() => {
+                    setRejections((prevValue) => prevValue + 1);
+                  }}
+                >
+                  {noText[rejections]}
+                </button>
+              )}
+            </p>
+          </CardContent>
+        )}
+        {yes && <CardContent className='text-center text-5xl'>🥰</CardContent>}
       </Card>
-      <a href="https://www.instagram.com/reel/C24CAVouAAZ/?igsh=NzlnOGQzY21kcTBl">
+      {/* <a href="https://www.instagram.com/reel/C24CAVouAAZ/?igsh=NzlnOGQzY21kcTBl">
         Inspiratoin
-      </a>
+      </a> */}
     </PageLayout>
   );
 };
