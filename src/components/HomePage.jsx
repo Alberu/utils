5dd10eb43b9c3c2eafdaed882bee6d846a8d8074
Alberu <treblabr@gmail.com>
2025-04-23 14:33:28 +0100

    added a filter search

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,18 +1,34 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { pages } from "@/utils";
 import { Link } from "react-router-dom";
+import { Input } from "./ui/input";
+import { useState } from "react";
 
 const HomePage = () => {
+  const [filterString, setFilterString] = useState("");
+
   const modules = import.meta.glob("@/pages/*.jsx", {
     eager: true,
     import: "meta",
   });
+  console.log(modules)
+
+  const filteredModules = Object.entries(modules).filter(([path, meta]) => {
+    if (meta.title.toLowerCase().includes(filterString)) return true
+    return false
+  })
 
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-8">Utils Collection</h1>
+      <Input
+        type="text"
+        value={filterString}
+        onChange={(e) => setFilterString(e.target.value)}
+        className="shadow"
+      />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {Object.entries(modules).map(([path, meta]) => {
+        {filteredModules.map(([path, meta]) => {
           return (
             <Link key={path} to={`/${meta.title}`} className="block">
               <Card>
