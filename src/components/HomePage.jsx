df5c0ca30e04edc790eb0c296b63239d00b5146e
Alberu <treblabr@gmail.com>
2025-04-23 14:37:40 +0100

    added some styling to the search bar

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -19,13 +19,14 @@ const HomePage = () => {
   })
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 space-y-9">
       <h1 className="text-4xl font-bold mb-8">Utils Collection</h1>
       <Input
         type="text"
         value={filterString}
         onChange={(e) => setFilterString(e.target.value)}
-        className="shadow"
+        className="text-center shadow"
+        placeholder="Use me to search"
       />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {filteredModules.map(([path, meta]) => {
